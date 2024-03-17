fe1f5c489d86e287b430e404c657934ed144d396
yusrA312 <yusra2yahya@gmail.com>
2024-03-17 15:47:56 -0700

    done

--- models/amenity.py
+++ models/amenity.py
@@ -3,12 +3,12 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
+    Attributes:
+        name: input name
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
