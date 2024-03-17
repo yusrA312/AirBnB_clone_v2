a03702435dab5290056316b1e439e4a89893c2f9
esrAA312 <esraayahya312@gmail.com>
2024-03-17 04:13:50 -0700

    done

--- models/state.py
+++ models/state.py
@@ -3,6 +3,12 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    This is the class for State
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete, delete-orphan")
