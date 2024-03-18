671131df9016ed85a580d5537c972ff4855a4bbc
esrAA312 <esraayahya312@gmail.com>
2024-03-18 16:28:12 -0700

    done

--- models/amenity.py
+++ models/amenity.py
@@ -7,8 +7,6 @@ from sqlalchemy import Column, String
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
-    Attributes:
-        name: input name
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
--- models/place.py
+++ models/place.py
@@ -6,6 +6,16 @@ from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from os import getenv
 import models
 
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
+
 
 class Place(BaseModel, Base):
     """
