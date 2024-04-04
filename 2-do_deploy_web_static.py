52fe1bb49e79740b56f7b690737319155ccfa940
yusrA312 <yusra2yahya@gmail.com>
2024-04-04 04:29:12 -0700

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+an archive
 """
 
-from fabric.api import put, run, env
-from os.path import exists
+from datetime import datetime
+from fabric.api import *
+import os
 
-env.hosts = ['54.84.245.120', '3.84.238.226']
+env.hosts = ["3.84.238.226", "54.84.245.120"]
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not exists(archive_path):
-        return False
-
-    try:
-        file_name = archive_path.split("/")[-1]
-        no_extension = file_name.split(".")[0]
-        path = "/data/web_static/releases/"
-
-        put(archive_path, '/tmp/')
-        run(f'mkdir -p {path}{no_extension}/')
-        run(f'tar -xzf /tmp/{file_name} -C {path}{no_extension}/')
-        run(f'rm /tmp/{file_name}')
-        run(f'mv {path}{no_extension}/web_static/* {path}{no_extension}/')
-        run(f'rm -rf {path}{no_extension}/web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {path}{no_extension}/ /data/web_static/current')
-        
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run(f"sudo mkdir -p {newest_version}")
+        run(f"sudo tar -xzf {archived_file} -C {newest_version}/")
+        run(f"sudo rm {archived_file}")
+        run(f"sudo mv {newest_version}/web_static/* {newest_version}")
+        run(f"sudo rm -rf {newest_version}/web_static")
+        run(f"sudo rm -rf /data/web_static/current")
+        run(f"sudo ln -s {newest_version} /data/web_static/current")
         return True
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
+
+    return False
+
+if __name__ == "__main__":
+    do_deploy(archive_path)
