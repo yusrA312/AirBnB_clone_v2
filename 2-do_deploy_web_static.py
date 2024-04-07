721af74e413f593ce3d06075308268eca4d193e6
yusrA312 <yusra2yahya@gmail.com>
2024-04-07 11:02:37 -0700

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,30 @@
 #!/usr/bin/python3
 """
-do_deploy(): Distributes an archive to a web server
+archive to the web servers
 """
 
-from fabric.operations import local, run, put, sudo
-from datetime import datetime
-import os
-from fabric.api import env
-import re
+from fabric.api import put, run, env
+from os.path import exists
 
-env.hosts = ["3.84.238.226", "54.84.245.120"]
+env.hosts = ["100.25.110.130", "3.86.7.109"]
 
 
 def do_deploy(archive_path):
-    """Function to distribute an archive to a server"""
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-    rex = r"^versions/(\S+).tgz"
-    match = re.search(rex, archive_path)
-    filename = match.group(1)
-    res = put(archive_path, "/tmp/{}.tgz".format(filename))
-    if res.failed:
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}{}/".format(path, no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file_n, path, no_ext))
+        run("rm /tmp/{}".format(file_n))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext))
+        run("rm -rf {}{}/web_static".format(path, no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
+        return True
+    except:
         return False
-    res = run("mkdir -p /data/web_static/releases/{}/".format(filename))
-    if res.failed:
-        return False
-    res = run(
-        "tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
-            filename, filename
-        )
-    )
-    if res.failed:
-        return False
-    res = run("rm /tmp/{}.tgz".format(filename))
-    if res.failed:
-        return False
-    res = run(
-        "mv /data/web_static/releases/{}"
-        "/web_static/* /data/web_static/releases/{}/".format(filename, filename)
-    )
-    if res.failed:
-        return False
-    res = run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
-    if res.failed:
-        return False
-    res = run("rm -rf /data/web_static/current")
-    if res.failed:
-        return False
-    res = run(
-        "ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename)
-    )
-    if res.failed:
-        return False
-    print("New version deployed!")
-    return True
