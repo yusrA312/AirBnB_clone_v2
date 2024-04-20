4b8472ef290319c375fd63a11969fc8ae645c45c
yusrA312 <yusra2yahya@gmail.com>
2024-04-20 10:30:11 -0700

    done

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,40 +8,34 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def Hiesraa():
-    """Display Hello HBNB!"""
+def Hiyusra():
     return 'Hello HBNB!'
 
 
 @app.route("/hbnb", strict_slashes=False)
-def HB():
-    """Display HBNB"""
+def yHB():
     return 'HBNB'
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def CC(text):
-    """Display C followed by the value of the text variable"""
-    t = text.replace('_', ' ')
-    return 'C ' + t
+def CyC(text):
+    xt = text.replace('_', ' ')
+    return 'C ' + xt
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def COOL(text='is cool'):
-    """display Python followed by the value of the text variable"""
+def YOOL(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
-def NA(n):
-    """display n is a number"""
+def NyA(n):
     return f'{n} is a number'
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def nue(n):
-    """This is a function to deploy the jinja template"""
+def ynue(n):
     return render_template("5-number.html", n=n)
 
 
